ffda1b61f603cf19c6fdc2078486b195f4265d7e
arcoard <arcoard+github@gmail.com>
2015-02-15 13:11:53 -0800

    Tidied up gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -21,13 +21,6 @@ gulp.task('build', ['clean'], function() {
     gulp.src('./src/index.html')
         .pipe(gulp.dest('./dist'));
 
-
-
-        //first two are es6
-        // gulp.src( ["src/app.js"] )
-        // .pipe(to5())
-        // .pipe(gulp.dest('./dist'));
-
         browserify({
         entries: './src/app.js',
         debug: true
@@ -36,10 +29,7 @@ gulp.task('build', ['clean'], function() {
         .bundle()
         .pipe(source('app.js'))
         .pipe(gulp.dest('./dist'));
-        
-        // gulp.src( ["src/models/*.js"] )
-        // .pipe(to5())
-        // .pipe(gulp.dest('./dist/models/'));
+
         browserify({
         entries: ['./src/models/Canvas.js', './src/models/UIController.js', './src/models/ImageElementHandler.js'],
         // entries: './src/models/*.js'
@@ -56,16 +46,6 @@ gulp.task('build', ['clean'], function() {
 });
 
 
-// gulp.task("default", function () {
-//   return gulp.src(["src/*.es6"])
-//   // .pipe(browserify())
-    // .pipe(to5())
-//     .pipe(rename(function(path){
-//         path.extname = '.js'
-//     }))
-//     .pipe(gulp.dest("dist/"));
-// });
-
 gulp.task('watch', function () {
     gulp.watch("src/**/*.js", ['build', browserSync.reload]);
     gulp.watch("src/*.html", ['build', browserSync.reload]);
@@ -80,53 +60,3 @@ gulp.task('serve', ['build', 'watch'], function () {
 });
 
 gulp.task('default', ['serve']);
-
-
-// var gulp = require('gulp');
-// var source = require('vinyl-source-stream');
-// var browserify = require("browserify");
-// var to5ify = require("6to5ify");
-
-// gulp.task('build', function() {
-//     return browserify({
-//         entries: './assets/scripts/main6.js',
-//         debug: true
-//         })
-//         .transform(to5ify)
-//         .bundle()
-//         .pipe(source('bundle.js'))
-//         .pipe(gulp.dest('./assets/static/'));
-// });
-
-
-
-// var getBundleName = function () {
-//   // var version = require('./package.json').version;
-//   var name = require('./package.json').name;
-//   return name + 'min';
-//   // return version + '.' + name + '.' + 'min';
-// };
-
-// // gulp.task('browserifyjs', ['build'], function() {
-// gulp.task('browserifyjs', function() {
-
-
-//   var bundler = browserify({
-//     entries: ['./dist/app.js'],
-//     debug: true
-//   });
-
-//   var bundle = function() {
-//     return bundler
-//       .bundle()
-//       .pipe(source(getBundleName() + '.js'))
-//       .pipe(buffer())
-//       .pipe(sourcemaps.init({loadMaps: true}))
-//         // Add transformation tasks to the pipeline here.
-//         // .pipe(uglify())
-//       .pipe(sourcemaps.write('./'))
-//       .pipe(gulp.dest('./dist/js/'));
-//   };
-
-//   // return bundle();
-// });
