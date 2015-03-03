c2a9d643a5bcae4c58dcf6f7b24d1fde336fc1e6
arcoard <arcoard+github@gmail.com>
2015-03-02 21:09:36 -0800

    Made width customizable with object instantiation.

--- src/app.js
+++ src/app.js
@@ -6,9 +6,12 @@ class API{
 
         if (typeof options.target === "undefined") throw "Required property in option object missing: `target`";
         
+        var width = options.width ? options.width : 600;
+        var height = options.height? options.height : 300;
+
         this.$container = $('#' + options.target);
 
-        var canvasTemplate = `<canvas id="es6-bootstrap-canvas" width="600" height="300"></canvas>`
+        var canvasTemplate = `<canvas id="es6-bootstrap-canvas" width="${width}" height="${height}"></canvas>`
         this.$container.append(canvasTemplate);
         this.canvas = new Canvas('es6-bootstrap-canvas');
 
@@ -20,7 +23,10 @@ class API{
 
 document.addEventListener("DOMContentLoaded", function(event) { 
     window.api = new API({
-        target: 'es6-bootstrap-container'
+        target: 'es6-bootstrap-container',
+        // defaults:
+        // width: 600,
+        // height: 300
     });
     //This is the 'init', and also I've made canvas a global for dev.
     // window.canvas = new Canvas('canvas');
