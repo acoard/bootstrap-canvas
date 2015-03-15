5cda878e3404d8743df06547fea1ded6fb5edd5c
arcoard <arcoard+github@gmail.com>
2015-03-15 11:02:24 -0700

    Tidied up instnatiator of API because we're putting options in the jQuery logic.

--- src/app.js
+++ src/app.js
@@ -3,22 +3,14 @@ import UIController from "./models/UIController";
 
 // @todo - rename as 'BootstrapCanvas' and make a jQuery plugin
 class API{
-    constructor(options){
+    constructor(targetElement, options){
 
-        if (typeof options.target === "undefined") throw "Required property in option object missing: `target`";
-
-        let DEFAULT_WIDTH = 800; //px
-        let DEFAULT_HEIGHT = 300; //px
         let CANVAS_WIDTH_DIFFERENCE = 300; //diff between the container width and canvas width;
-        
-        var width = options.width ? options.width : DEFAULT_WIDTH;
-        var height = options.height? options.height : DEFAULT_HEIGHT;
 
+        this.$container = $(targetElement);
 
-        this.$container = $('#' + options.target);
-
-        var canvasWidth =  width - CANVAS_WIDTH_DIFFERENCE;
-        var canvasHeight = height;
+        var canvasWidth =  options.width - CANVAS_WIDTH_DIFFERENCE;
+        var canvasHeight = options.height;
 
         var canvasTemplate = `<canvas id="es6-bootstrap-canvas" width="${canvasWidth}" height="${canvasHeight}"></canvas>`
         this.$container.append(canvasTemplate);
@@ -27,7 +19,7 @@ class API{
             'position' : 'relative',
         });
         this.canvas = new Canvas('es6-bootstrap-canvas');
-        this.ui = new UIController({canvas: this.canvas, container: this.$container, layerWidth: CANVAS_WIDTH_DIFFERENCE, layerHeight: height});
+        this.ui = new UIController({canvas: this.canvas, container: this.$container, layerWidth: CANVAS_WIDTH_DIFFERENCE, layerHeight: options.height});
 
         //todo, refactor this tight coupling out
         //right now it's needed for canvas.importImageToCanvas calling ui.drawImagesList
@@ -92,10 +84,80 @@ document.addEventListener("DOMContentLoaded", function(event) {
      *
      * http://getbootstrap.com/javascript/
      */
+    
+
+    ;(function ( $, window, document, undefined ) {
+ 
+        // undefined is used here as the undefined global
+        // variable in ECMAScript 3 and is mutable (i.e. it can
+        // be changed by someone else). undefined isn't really
+        // being passed in so we can ensure that its value is
+        // truly undefined. In ES5, undefined can no longer be
+        // modified.
+     
+        // window and document are passed through as local
+        // variables rather than as globals, because this (slightly)
+        // quickens the resolution process and can be more
+        // efficiently minified (especially when both are
+        // regularly referenced in our plugin).
+     
+        // Create the defaults once
+        var pluginName = "bootstrapCanvas",
+            defaults = {
+                'width': "800",
+                'height': '300'
+            };
+     
+        // The actual plugin constructor
+        function Plugin( element, options ) {
+            this.element = element;
+     
+            // jQuery has an extend method that merges the
+            // contents of two or more objects, storing the
+            // result in the first object. The first object
+            // is generally empty because we don't want to alter
+            // the default options for future instances of the plugin
+            this.options = $.extend( {}, defaults, options) ;
+     
+            this._defaults = defaults;
+            this._name = pluginName;
+     
+            this.init();
+        }
+     
+        Plugin.prototype.init = function () {
+            window.apiInstance = new API(this.element, this.options); 
+            // Place initialization logic here
+            // We already have access to the DOM element and
+            // the options via the instance, e.g. this.element
+            // and this.options
+        };
+     
+        // A really lightweight plugin wrapper around the constructor,
+        // preventing against multiple instantiations
+        $.fn[pluginName] = function ( options ) {
+            return this.each(function () {
+                if ( !$.data(this, "plugin_" + pluginName )) {
+                    $.data( this, "plugin_" + pluginName,
+                    new Plugin( this, options ));
+                }
+            });
+        }
+ 
+    })( jQuery, window, document );
+    
 
 
-    window.api = new API({
-        target: 'es6-bootstrap-container',
-        // defaults:
-    });
+     // (function( $ ){
+     //    $.fn.bootstrapCanvas = API;
+     //   // $.fn.bootstrapCanvas = function () {
+         
+     //   // };
+     // })( jQuery );
+
+    // window.api = new API({
+    //     target: 'es6-bootstrap-container',
+    //     // defaults:
+    // });
+    $('#es6-bootstrap-container').bootstrapCanvas()
 });
